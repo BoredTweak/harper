9c877b19be1926d006399e80b070468a97f54285
BoredTweak <alex.elia37@gmail.com>
2021-09-17 13:49:03 -0700

    Adding output argument supporting console and json output. Defaults to json.

--- harper.py
+++ harper.py
@@ -18,20 +18,30 @@ def _default(self, obj):
 _default.default = JSONEncoder.default  # Save unmodified default.
 JSONEncoder.default = _default # Replace it.
 
-def writeOutput(results: dict):
-    json_object = json.dumps(results, indent = 4) 
-    with open('output.json', 'w') as file:
-        file.write(json_object)
+def writeOutput(results: dict, outputMethod: str):
+    if(outputMethod == 'json'):
+        json_object = json.dumps(results, indent = 4) 
+        with open('output.json', 'w') as file:
+            file.write(json_object)
+    if(outputMethod == 'console'):
+        print(json.dumps(results, indent = 4))
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--token", required=True, help="GitHub Personal Access Token")
     parser.add_argument("--since", help="(Optional) Filter results to only items since date in format yyyy-MM-dd")
+    parser.add_argument("--output", help="(Optional) Specify where to output. Options: [console, json]")
     args = parser.parse_args()
     if not args.token:
         print('Please retry with a GitHub PAT token specified with the `--token TOKEN` argument')
         return
-    
+    outputMethod = args.output
+    if(outputMethod is None):
+        outputMethod = 'json'
+    if(outputMethod != 'json' and outputMethod != 'console'):
+        print('Please retry with a valid output argument `--output json` or `--output console')
+        return
+
     service = GitHubService(args.token)
     results = []
     config = Configuration()
@@ -39,7 +49,7 @@ def main():
         result = service.generateNotification(repository, sinceDate=args.since)
         if(result is not None):
             results.append(result)
-    writeOutput(results)
+    writeOutput(results, outputMethod)
 
 if __name__ == "__main__":
     main()
